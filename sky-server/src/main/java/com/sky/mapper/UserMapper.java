68c3fb15b46cede1ea5d35aadfe050abe77f48c8
shennn <1057248626@qq.com>
2026-03-12 23:19:37 +0800

    微信支付业务开发完毕

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -2,10 +2,14 @@ package com.sky.mapper;
 
 import com.sky.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     User getByOpenid(String openid);
 
     void save(User user);
+
+    @Select(" select * from user where id=#{userId}")
+    User getById(Long userId);
 }
